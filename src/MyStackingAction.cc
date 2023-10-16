b9cf692a6cce2ee8f8ab4051f9ddb15153251139
mkondzie <141751789+mkondzie@users.noreply.github.com>
2023-10-16 19:37:32 +0200

    added killing of scattered gamma tracks
    random seeds are still not random

--- src/MyStackingAction.cc
+++ src/MyStackingAction.cc
@@ -1,10 +1,8 @@
 #include "MyStackingAction.h"
-
 #include "G4Track.hh"
+
 #include "G4Electron.hh"
 #include "G4Positron.hh"
-
-
 
 G4ClassificationOfNewTrack
 MyStackingAction::ClassifyNewTrack(const G4Track* track)
@@ -12,11 +10,12 @@ MyStackingAction::ClassifyNewTrack(const G4Track* track)
   //keep primary particle
   if (track->GetParentID() == 0) return fUrgent;
 
+
   //kill secondary electrons and positrons
   if ((track->GetDefinition() == G4Electron::Electron()) || (track->GetDefinition() == G4Positron::Positron())) return fKill;
-  
+
+
   else return fUrgent;
 }
-
 
 
--- src/MySteppingAction.cc
+++ src/MySteppingAction.cc
@@ -1,8 +1,9 @@
 #include "MySteppingAction.h"
 #include "G4RunManager.hh"
+
 #include "G4SystemOfUnits.hh"
 #include "CLHEP/Units/PhysicalConstants.h"
-
+#include "G4Gamma.hh"
 
 MySteppingAction::MySteppingAction(MyEventAction* eventAction) {
     fEventAction = eventAction;
@@ -13,21 +14,33 @@ MySteppingAction::~MySteppingAction() { }
 
 void MySteppingAction::UserSteppingAction(const G4Step* step)
 {
-
-    /*G4Track* track = step->GetTrack();
-    
-    G4String processName = step->GetTrack()->GetCreatorProcess()->GetProcessName();
-    G4cout << "Process Name :" << processName << G4endl;
-
-    G4String particleName = track->GetDefinition()->GetParticleName();
-    G4cout << "particle Name:" << particleName << G4endl;
+    //kill scattered gammas
+    G4Track* track = step->GetTrack();
 
     G4double particleEnergy = track->GetKineticEnergy();
-    G4cout << "particle Energy:" << particleEnergy << G4endl;
-
     G4double theta = 1.e-4;
     G4double E0 = 10 * MeV;
     G4double E = 1 / (((1 - cos(theta)) / CLHEP::electron_mass_c2) + 1 / E0);
+
+       
+
+    if ((track->GetDefinition() == G4Gamma::Gamma()) && (particleEnergy < E))
+    {
+        track->SetTrackStatus(fKillTrackAndSecondaries);
+    }
+
+
+
+
+    G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
+        const MyDetectorConstruction* detectorConstruction = static_cast<const MyDetectorConstruction*>
+        (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+
+
+    /*
+    G4String processName = step->GetTrack()->GetCreatorProcess()->GetProcessName();
+
+    G4String particleName = track->GetDefinition()->GetParticleName();
 
     G4int parentId = track->GetParentID();
 
@@ -50,29 +63,15 @@ void MySteppingAction::UserSteppingAction(const G4Step* step)
 
     */
 
-
-   // G4Track* track = step->GetTrack();
-
-    //G4String processName = step->GetTrack()->GetCreatorProcess()->GetProcessName();
-   // const G4ParticleDefinition* particleDef = track->GetParticleDefinition();
-
-
-
-    
    // if (particleDef->GetPDGCharge() == 0.0)
    // {
-        
-
        // if ((processName == "phot") || (processName == "conv")) {
-
         //    track->SetTrackStatus(fKillTrackAndSecondaries);
        // }
 
 
-     G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
-     const MyDetectorConstruction* detectorConstruction = static_cast<const MyDetectorConstruction*>
-     (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
 
+    
       
 
 
