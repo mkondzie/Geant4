3382dcf47757b6c17db67bd2414923e80ec374f2
mkondzie <141751789+mkondzie@users.noreply.github.com>
2023-11-11 09:39:48 +0100

    Update MyRunAction.cc

--- src/MyRunAction.cc
+++ src/MyRunAction.cc
@@ -6,48 +6,26 @@
 #include "Randomize.hh"
 
 MyRunAction::MyRunAction() {
-
-	
 	
 }
-
-
 
 MyRunAction::~MyRunAction() {
 
 }
 
-
-
 void MyRunAction::BeginOfRunAction(const G4Run*) {
-
 
 		G4AnalysisManager* man = G4AnalysisManager::Instance();
 
-
-
 		man->OpenFile("output.root");
-
-
-		
-
+	
 		man->CreateNtuple("Hits", "Hits");
 		man->CreateNtupleDColumn("fX"); //XY position
 		man->CreateNtupleDColumn("fY"); //Z fixed to 60.*m
 		man->CreateNtupleDColumn("fE");
-		//man->CreateNtupleIColumn("fParentID");
-		//man->CreateNtupleDColumn("fZ");
-		//man->CreateNtupleIColumn("fTrackID"); 
 		man->FinishNtuple(0);
-
-
-
-		//G4RunManager::GetRunManager()->SetRandomNumberStore(true);
-		
 		
 }
-
-
 
 void MyRunAction::EndOfRunAction(const G4Run*) {
 
