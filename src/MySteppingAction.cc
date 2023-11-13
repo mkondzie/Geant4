c763d076da1fa9a666b0d252be337b3da2a5dc47
mkondzie <141751789+mkondzie@users.noreply.github.com>
2023-11-13 19:40:28 +0100

    Update MySteppingAction.cc

--- src/MySteppingAction.cc
+++ src/MySteppingAction.cc
@@ -38,7 +38,7 @@ void MySteppingAction::UserSteppingAction(const G4Step* step)
     }
 
     
-    G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
+    G4LogicalVolume* volume = preStepPoint->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
         const MyDetectorConstruction* detectorConstruction = static_cast<const MyDetectorConstruction*>
         (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
 }
