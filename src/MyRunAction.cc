aad1f4ba5bfe7d985c9c24bde3686e9af8932499
mkondzie <141751789+mkondzie@users.noreply.github.com>
2023-11-16 21:50:43 +0100

    Update MyRunAction.cc

--- src/MyRunAction.cc
+++ src/MyRunAction.cc
@@ -6,38 +6,23 @@
 #include "Randomize.hh"
 
 MyRunAction::MyRunAction() {
-
-	
 	
 }
-
-
 
 MyRunAction::~MyRunAction() {
 
 }
 
-
-
 void MyRunAction::BeginOfRunAction(const G4Run*) {
-
 
 		G4AnalysisManager* man = G4AnalysisManager::Instance();
 
-
-
 		man->OpenFile("output.root");
 
-
-		
-
 		man->CreateNtuple("Hits", "Hits");
-		man->CreateNtupleDColumn("fX"); //XY position
-		man->CreateNtupleDColumn("fY"); //Z fixed to 60.*m
+		man->CreateNtupleDColumn("fX"); 
+		man->CreateNtupleDColumn("fY"); 
 		man->CreateNtupleDColumn("fE");
-		//man->CreateNtupleIColumn("fParentID");
-		//man->CreateNtupleDColumn("fZ");
-		//man->CreateNtupleIColumn("fTrackID"); 
 		man->FinishNtuple(0);
 		
 		man->CreateNtuple("Scattering", "Scattering");
@@ -45,14 +30,9 @@ void MyRunAction::BeginOfRunAction(const G4Run*) {
 		man->CreateNtupleDColumn("fEi");
 		man->CreateNtupleDColumn("fEf");
 		man->FinishNtuple(1);
-
-
-		//G4RunManager::GetRunManager()->SetRandomNumberStore(true);
-		
-		
+	
+		//G4RunManager::GetRunManager()->SetRandomNumberStore(true);	
 }
-
-
 
 void MyRunAction::EndOfRunAction(const G4Run*) {
 
