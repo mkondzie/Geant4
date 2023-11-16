030bdeee7978b155a7c075bf194225d3004f6281
mkondzie <141751789+mkondzie@users.noreply.github.com>
2023-11-16 21:49:01 +0100

    Update MySensitiveDetector.cc

--- src/MySensitiveDetector.cc
+++ src/MySensitiveDetector.cc
@@ -3,19 +3,15 @@
 #include "G4AnalysisManager.hh"
 #include "G4RunManager.hh"
 
-
-
 MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name) 
 { }
 
 MySensitiveDetector::~MySensitiveDetector()
-{}
-
+{ }
 
 G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist){
   G4Track *track = aStep->GetTrack();
   track->SetTrackStatus(fStopAndKill);
-
 
   G4StepPoint *preStepPoint = aStep->GetPreStepPoint(); 
   G4StepPoint *postStepPoint = aStep->GetPostStepPoint(); 
@@ -27,23 +23,15 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhis
   G4double energy = track->GetKineticEnergy();
   G4int trackID = track->GetTrackID();
   G4int parentID = track->GetParentID();
-
-  
+ 
   G4AnalysisManager* man = G4AnalysisManager::Instance();
  
-
   man->FillNtupleDColumn(0, position.x());
   man->FillNtupleDColumn(1, position.y());
   man->FillNtupleDColumn(2, energy);
-  //man->FillNtupleIColumn(3, parentID);
-  //man->FillNtupleDColumn(4, position.z());
-  //man->FillNtupleIColumn(5, trackID);
   
-
   man->AddNtupleRow(0);
   
- 
-
   return true; 
 }
 
