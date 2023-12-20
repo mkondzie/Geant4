890decc77abe2fb6f7cc1e8e6232d3174d808987
mkondzie <141751789+mkondzie@users.noreply.github.com>
2023-12-20 22:27:20 +0100

    cleaned up the code

--- include/MyStackingAction.h
+++ include/MyStackingAction.h
@@ -4,11 +4,6 @@
 #include "G4UserStackingAction.hh"
 #include "globals.hh"
 
-
-/// Stacking action class : manage the newly generated particles
-/// One wishes do not track secondary electrons and positrons.
-/// Therefore one kills it immediately, before created particles will be put in a stack.
-
 class MyStackingAction : public G4UserStackingAction
 {
 public:
@@ -17,4 +12,5 @@ public:
 
     G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track*) override;
 };
+
 #endif
