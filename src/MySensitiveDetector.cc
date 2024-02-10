cf1c75bda1d6cb5f00674aa02704cbfddd4c3a8e
mkondzie <141751789+mkondzie@users.noreply.github.com>
2024-02-10 10:28:29 +0100

    Added histograms counting hits

--- src/MySensitiveDetector.cc
+++ src/MySensitiveDetector.cc
@@ -1,36 +1,50 @@
 #include "MySensitiveDetector.h"
 #include "G4Step.hh"
 #include "G4AnalysisManager.hh"
+#include "MyPrimaryGenerator.h"
 #include "G4RunManager.hh"
+#include "G4SystemOfUnits.hh"
 
-MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name) 
-{ }
+MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name) { }
 
-MySensitiveDetector::~MySensitiveDetector()
-{ }
+MySensitiveDetector::~MySensitiveDetector() { }
 
-G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist){
+G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
+{
+  const MyPrimaryGenerator *primaryGenerator = static_cast<const MyPrimaryGenerator *>(G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
 
   G4Track *track = aStep->GetTrack();
   track->SetTrackStatus(fStopAndKill);
 
-  G4StepPoint *preStepPoint = aStep->GetPreStepPoint(); 
-  G4StepPoint *postStepPoint = aStep->GetPostStepPoint(); 
+  G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
+  G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
 
   G4ThreeVector prePosition = preStepPoint->GetPosition();
   G4ThreeVector postPosition = postStepPoint->GetPosition();
 
   G4ThreeVector position = (prePosition + postPosition) / 2;
   G4double energy = track->GetKineticEnergy();
-  
-  G4AnalysisManager* man = G4AnalysisManager::Instance();
- 
-  man->FillNtupleDColumn(0, position.x());
-  man->FillNtupleDColumn(1, position.y());
-  man->FillNtupleDColumn(2, energy);
-  man->AddNtupleRow(0);
-  
-  return true; 
 
+  G4AnalysisManager *man = G4AnalysisManager::Instance();
+
+  if (track->GetParentID() == 0)
+  {
+    man->FillH1(1, 0.5);
+  }
+
+  if (energy < primaryGenerator->GetPrimaryEnergy())
+  {
+    man->FillH1(2, 0.5);
+    man->FillNtupleDColumn(0, position.x());
+    man->FillNtupleDColumn(1, position.y());
+    man->FillNtupleDColumn(2, energy);
+    man->AddNtupleRow(0);
+  }
+
+  if (man->GetH1(3)->entries() == 0)
+  {
+    man->FillH1(3, 0.5, primaryGenerator->GetPrimaryEnergy());
+  }
+
+  return true;
 }
-
