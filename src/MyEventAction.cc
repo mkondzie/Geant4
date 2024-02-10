b50e95a4abaed3b5f02a37db48bbebb4092b868e
mkondzie <141751789+mkondzie@users.noreply.github.com>
2024-02-10 09:59:31 +0100

    Added a histogram counting primaries

--- src/MyEventAction.cc
+++ src/MyEventAction.cc
@@ -1,13 +1,18 @@
 #include "MyEventAction.h"
 #include "MyRunAction.h"
-
 #include "G4SystemOfUnits.hh"
 #include "G4AnalysisManager.hh"
 
-MyEventAction::MyEventAction(MyRunAction*) { }
+MyEventAction::MyEventAction(MyRunAction *) {}
 
-MyEventAction::~MyEventAction() { }
+MyEventAction::~MyEventAction() {}
 
-void MyEventAction::BeginOfEventAction(const G4Event*) { }
+void MyEventAction::BeginOfEventAction(const G4Event *) {}
 
-void MyEventAction::EndOfEventAction(const G4Event*) { }
+void MyEventAction::EndOfEventAction(const G4Event *event)
+{
+
+    G4AnalysisManager *man = G4AnalysisManager::Instance();
+    man->FillH1(0, 0.5, event->GetPrimaryVertex()->GetNumberOfParticle());
+}
+
