ad39ca444236ab30495b76493d05984880855dae
mkondzie <141751789+mkondzie@users.noreply.github.com>
2024-02-10 10:41:52 +0100

    Added fCylinderHalfLength

--- include/MyDetectorConstruction.h
+++ include/MyDetectorConstruction.h
@@ -4,27 +4,25 @@
 #include "G4VUserDetectorConstruction.hh"
 #include "G4LogicalVolume.hh"
 
-
 class G4VPhysicalVolume;
 class G4LogicalVolume;
 
 class MyDetectorConstruction : public G4VUserDetectorConstruction 
 {
 public: 
-
   MyDetectorConstruction();
 
   ~MyDetectorConstruction();
 
   G4LogicalVolume* GetScoringVolume() const { return fScoringVolume; }
-
+  G4double GetCylinderHalfLength() const { return fCylinderHalfLength; }
   virtual G4VPhysicalVolume *Construct();
 
 private:
   G4LogicalVolume * logicDet;
   virtual void ConstructSDandField();
-     
 
   G4LogicalVolume *fScoringVolume;
+  G4double fCylinderHalfLength; 
 };
 #endif
