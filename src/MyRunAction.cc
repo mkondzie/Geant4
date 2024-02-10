007b57bedcf773b445426a3949bbc8f2aeb5b0ac
mkondzie <141751789+mkondzie@users.noreply.github.com>
2024-02-10 10:16:04 +0100

    Added counting histograms and merging

--- src/MyRunAction.cc
+++ src/MyRunAction.cc
@@ -2,42 +2,49 @@
 #include "MyActionInitialization.h"
 #include "G4AnalysisManager.hh"
 #include "MyPrimaryGenerator.h"
+#include "MyDetectorConstruction.h"
 #include "G4RunManager.hh"
 #include "Randomize.hh"
 
-MyRunAction::MyRunAction() 
-{ }
+MyRunAction::MyRunAction()
+{
+	G4AnalysisManager *man = G4AnalysisManager::Instance();
+	man->SetNtupleMerging(true);
+	man->Reset();
+	man->CreateH1("Primaries", "", 1, 0, 1);
+	man->CreateH1("PrimaryHits", "", 1, 0, 1);
+	man->CreateH1("ScatteredHits", "", 1, 0, 1);
 
-MyRunAction::~MyRunAction() 
-{ }
+	man->CreateH1("PrimaryEnergy", "", 1, 0, 1);
+	man->CreateH1("CylinderHalfLength", "", 1, 0, 1);
 
-void MyRunAction::BeginOfRunAction(const G4Run*) {
+	man->CreateNtuple("Hits", "Hits");
+	man->CreateNtupleDColumn("fX");
+	man->CreateNtupleDColumn("fY");
+	man->CreateNtupleDColumn("fE");
+	man->FinishNtuple(0);
 
-		G4AnalysisManager* man = G4AnalysisManager::Instance();
-
-		man->OpenFile("output.root");
-
-		man->CreateNtuple("Hits", "Hits");
-		man->CreateNtupleDColumn("fX"); 
-		man->CreateNtupleDColumn("fY"); 
-		man->CreateNtupleDColumn("fE");
-		man->FinishNtuple(0);
-		
-		man->CreateNtuple("Scattering", "Scattering");
-		man->CreateNtupleDColumn("fTheta"); 
-		man->CreateNtupleDColumn("fpreMomentum");
-		man->CreateNtupleDColumn("fpostMomentum");
-		man->CreateNtupleDColumn("fX");
-		man->CreateNtupleDColumn("fY");
-		man->CreateNtupleDColumn("fZ");
-		man->FinishNtuple(1);
-		
+	man->CreateNtuple("Scattering", "Scattering");
+	man->CreateNtupleDColumn("fTheta");
+	man->CreateNtupleDColumn("fEi");
+	man->CreateNtupleDColumn("fEf");
+	man->CreateNtupleDColumn("fX");
+	man->CreateNtupleDColumn("fY");
+	man->CreateNtupleDColumn("fZ");
+	man->FinishNtuple(1);
 }
 
-void MyRunAction::EndOfRunAction(const G4Run*) {
+MyRunAction::~MyRunAction() { }
 
+void MyRunAction::BeginOfRunAction(const G4Run *)
+{
+	G4AnalysisManager *man = G4AnalysisManager::Instance();
+	man->OpenFile("output.root");
+}
+
+void MyRunAction::EndOfRunAction(const G4Run *)
+{
 	G4AnalysisManager *man = G4AnalysisManager::Instance();
 	man->Write();
-	man->CloseFile();
-
+	man->CloseFile(false);
 }
